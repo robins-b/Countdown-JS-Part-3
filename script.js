572c67d60547bddf3f01eacdc5f669338689f782
robins-b <surandarrobins.b@gmail.com>
2022-12-07 20:06:59 +0530

    readme

--- script.js
+++ script.js
@@ -6,28 +6,6 @@ let minutes = document.querySelector("#minutes");
 let hours = document.querySelector("#hours");
 let seconds = document.querySelector("#seconds");
 
-// function start() {
-//   num = input.value;
-//   let days = Math.floor(num / (3600 * 24));
-//   let hours = Math.floor((num % (3600 * 24)) / 3600);
-//   let minutes = Math.floor((num % 3600) / 60);
-//   let seconds = Math.floor(num % 60);
-//   if (days < 10) {
-//     days = "0" + days;
-//   }
-//   if (hours < 10) {
-//     hours = "0" + hours;
-//   }
-//   if (minutes < 10) {
-//     minutes = "0" + minutes;
-//   }
-//   if (seconds < 10) {
-//     seconds = "0" + seconds;
-//   }
-
-//   display.textContent = `${days}days:${hours}hrs:${minutes}mins:${seconds}sec`;
-// }
-
 function start() {
   if (input.value != "") {
     btn.classList.add("off");
